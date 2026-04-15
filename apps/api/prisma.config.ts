e958ce01f798f981be045c405403abf8c0263e62
Marcos Corazza <mcs.corazza@gmail.com>
2026-04-15 07:54:21 -0300

    feat: add supabase

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,14 +1,12 @@
-import "dotenv/config";
-import { defineConfig } from '@prisma/config';
-
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error("❌ Erro: A variável DATABASE_URL não foi encontrada no ambiente.");
-}
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
   datasource: {
-    url: databaseUrl,
+    url: env('DIRECT_URL'),
   },
 });
